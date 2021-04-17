7c0cdc388a2316ff532f78cb01dbd7d23892cd31
vicky <vicky.7dev@gmail.com>
2021-04-17 22:34:10 +0530

    rgb background changer completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+     
+    }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [color, setSolor] = useState(`30,30,30`);
+    const [notification, setNotification] = useState(false);
+    const changeColor = () => {
+        const getColor = () => {
+            return Math.floor(Math.random() * 255);
+        };
+        setSolor(`${getColor()},${getColor()},${getColor()}`);
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setNotification(false);
+            return () => {
+                window.clearInterval(interval);
+            };
+        }, 3000);
+    }, [notification]);
+
+    return (
+        <>
+            <GlobalStyles />
+            <AppContainer bgColor={color}>
+                <BtnContainer>
+                    <Btn onClick={changeColor}>change color</Btn>
+                </BtnContainer>
+                <Color
+                    onClick={() => {
+                        navigator.clipboard.writeText(color);
+                        setNotification(true);
+                    }}
+                >
+                    RBG Value: {color}
+                </Color>
+                {notification && (
+                    <Notification>Copied To Clipboard</Notification>
+                )}
+            </AppContainer>
+        </>
+    );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-color: rgb(${({ bgColor }) => bgColor});
+`;
+
+const BtnContainer = styled.div`
+    padding: 1rem;
+`;
+
+const Btn = styled.button`
+    padding: 0.5rem 1rem;
+    font-size: 1.2rem;
+    text-transform: capitalize;
+`;
+
+const Color = styled.h3`
+    cursor: pointer;
+`;
+
+const Notification = styled.p``;
